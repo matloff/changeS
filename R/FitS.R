cc3fe97a076fc326ee22093ebe9b72c7dcafe639
Norm Matloff <nsmatloff@ucdavis.edu>
2022-12-14 21:50:46 -0800

    refined

--- R/FitS.R
+++ R/FitS.R
@@ -1,4 +1,5 @@
-fit_wrapper <- function(data_in, x_col_index, y_col_index, slope_in=NULL) {
+
+fitS <- function(dataIn, xColIndex=NULL, yColIndex=NULL, slopeIn=NULL) {
 
   # Preliminary work (should be a smarter way to do this but haven't found yet)
   # library(nls.multstart)
@@ -6,11 +7,20 @@ fit_wrapper <- function(data_in, x_col_index, y_col_index, slope_in=NULL) {
     return((topLevel-bottomLevel)/(1+exp(-slope*(x-changePt))) + bottomLevel)
   }
   fitGenLogit_fixed <- function(topLevel, bottomLevel, changePt, x) {
-    return((topLevel-bottomLevel)/(1+exp(-slope_in*(x-changePt))) + bottomLevel)
+    return((topLevel-bottomLevel)/(1+exp(-slopeIn*(x-changePt))) + bottomLevel)
   }
 
-  # build the used data_frame
-  d <- data.frame(x=data_in[[x_col_index]], y=data_in[[y_col_index]])
+  # dataIn could be: a data frame; a numeric vector; a time series
+  # vector
+  
+  if (is.ts(dataIn)) {
+     d <- data.frame(x=time(dataIn),y=as.numeric(dataIn))
+  } else if (is.vector(dataIn)) {
+     d <- data.frame(x=1:length(dataIn),y=dataIn)
+  } else {
+     d <- data.frame(x=dataIn[[xColIndex]], y=dataIn[[yColIndex]])
+  }
+
   changePt_low = min(d$x)
   changePt_high = max(d$x)
   val_low = min(d$y)
@@ -18,16 +28,29 @@ fit_wrapper <- function(data_in, x_col_index, y_col_index, slope_in=NULL) {
 
   # fit
   ret <- NULL
-  if (!is.null(slope_in)) {
-    ret <- nls_multstart(y~fitGenLogit_fixed(topLevel, bottomLevel, changePt, x=x),
-                         d, iter=c(5,5,5), start_lower=c(val_low, val_low, changePt_low),
-                         start_upper=c(val_high, val_high, changePt_high), supp_errors="Y")
+  if (!is.null(slopeIn)) {
+    ret <- nls_multstart(y~fitGenLogit_fixed(topLevel, bottomLevel, changePt, 
+       x=x), d, iter=c(5,5,5), start_lower=c(val_low, val_low, changePt_low),
+       start_upper=c(val_high, val_high, changePt_high), supp_errors="Y")
   } else {
-    ret <- nls_multstart(y~fitGenLogit_unfixed(topLevel, bottomLevel, slope, changePt, x=x),
-                         d, iter=c(5,5,5,5), start_lower=c(val_low, val_low, 0, changePt_low),
-                         start_upper=c(val_high, val_high, 25, changePt_high), supp_errors="Y")
+    ret <- nls_multstart(y~fitGenLogit_unfixed(topLevel, bottomLevel, slope, 
+       changePt, x=x),
+       d, iter=c(5,5,5,5), start_lower=c(val_low, val_low, 0, changePt_low),
+       start_upper=c(val_high, val_high, 25, changePt_high), supp_errors="Y")
   }
+
   return(ret) # temporarily returning an nls obj
+
+###   retObj <- list()
+###   retObj$pars <- ret$m$getAllPars()
+###   class(retObj) <- 'fittedS'
+### 
+###   sm <- summary(ret)
+###   sm$cov.unscaled
+###   browser()
+### 
+###   retObj
+
 }
 
 
