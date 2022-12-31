398b0023193017a9ce1a026b710305ddad5b681a
Lan <jianglanniovo@gmail.com>
2022-12-31 13:42:02 -0800

    plot.fittedS bug fix and tiny manual changes
    1. plot.fittedS() now supports the return object generated by function calls with slope defined by users.
    2. plot.fittedS.Rd usage section format corrected.

--- R/FitS.R
+++ R/FitS.R
@@ -12,7 +12,7 @@ fitS <- function(dataIn, xColIndex=NULL, yColIndex=NULL, slopeIn=NULL) {
 
   # dataIn could be: a data frame; a numeric vector; a time series
   # vector
-  
+
   if (is.ts(dataIn)) {
      d <- data.frame(x=time(dataIn),y=as.numeric(dataIn))
   } else if (is.vector(dataIn)) {
@@ -29,11 +29,11 @@ fitS <- function(dataIn, xColIndex=NULL, yColIndex=NULL, slopeIn=NULL) {
   # fit
   ret <- NULL
   if (!is.null(slopeIn)) {
-    ret <- nls_multstart(y~fitGenLogit_fixed(postMean, preMean, changePt, 
+    ret <- nls_multstart(y~fitGenLogit_fixed(postMean, preMean, changePt,
        x=x), d, iter=c(5,5,5), start_lower=c(val_low, val_low, changePt_low),
        start_upper=c(val_high, val_high, changePt_high), supp_errors="Y")
   } else {
-    ret <- nls_multstart(y~fitGenLogit_unfixed(postMean, preMean, slope, 
+    ret <- nls_multstart(y~fitGenLogit_unfixed(postMean, preMean, slope,
        changePt, x=x),
        d, iter=c(5,5,5,5), start_lower=c(val_low, val_low, 0, changePt_low),
        start_upper=c(val_high, val_high, 25, changePt_high), supp_errors="Y")
@@ -46,41 +46,40 @@ fitS <- function(dataIn, xColIndex=NULL, yColIndex=NULL, slopeIn=NULL) {
 
   # covariance matrix of the par estimates
   sm <- summary(ret)
-  retObj$covMat <- sm$sigma^2 * sm$cov.unscaled 
-  
-  #standard error of the difference between pre and post (changepoint) means
-  retObj$stdErrorDiff <- sqrt(retObj$covMat[1,1] + retObj$covMat[2,2] - 2*retObj$covMat[1,2])
+  retObj$covMat <- sm$sigma^2 * sm$cov.unscaled
+  retObj$slopeGenerated <- is.null(slopeIn)
 
   class(retObj) <- c('fittedS')
   retObj
 
 }
 
-print.fittedS <- function(obj) 
+print.fittedS <- function(obj)
 {
    print('point estimates of the alpha_i')
    print(obj$pars)
    print('covariance matrix')
    print(obj$covMat)
-   print('standard error of the difference between pre-changepoint and post-changepoint means')
-   print(obj$stdErrorDiff)
 }
 
 plot.fittedS <- function(obj){
   #should return a ggplot object
-  require(ggplot2)
+  cpIndex <- 3
+  if (obj$slopeGenerated) {
+    cpIndex <- 4
+  }
   ggplot(data = obj$d, mapping = aes(x = x, y = y))+
     geom_point(alpha = .9, color = 'black')+
     geom_line(data = obj$d, mapping = aes(x = x, y = fitted, color = "S-fit"))+
     ggtitle("Changepoint Plot")+
     scale_color_manual(name = "Curve Type", values = "blue")+
-    geom_vline(xintercept = obj$par[[4]], color = "lightblue", linetype = "dashed")+
+    geom_vline(xintercept = obj$par[[cpIndex]], color = "lightblue", linetype = "dashed")+
     theme_minimal()+
-    annotate("text", x = obj$par[[4]], y = max(obj$d$y), label = str_glue("Estimated CP: {cp}", cp = round(obj$par[[4]],2)))+
-    # annotate("text", x = obj$par[[4]]+20, y = max(y)-150, label = str_glue('(std. error: {std.error})', std.error = round(std_error_cp,3)))+
+    annotate("text", x = obj$par[[cpIndex]], y = max(obj$d$y), label = str_glue("Estimated CP: {cp}", cp = round(obj$par[[cpIndex]],2)))+
+    # annotate("text", x = obj$par[[cpIndex]]+20, y = max(y)-150, label = str_glue('(std. error: {std.error})', std.error = round(std_error_cp,3)))+
     # scale_x_continuous(breaks = seq(min(obj$d$x),max(obj$d$x), by = 10))+
     theme(plot.title = element_text(face = "bold", size = 15, hjust= .5),
           plot.subtitle = element_text(hjust = .5))
-  
+
 }
 
